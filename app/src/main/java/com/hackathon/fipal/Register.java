12bf343d39479cd0b09456fcec37ddcbc61a69a9
kallash <36623100+kallash04@users.noreply.github.com>
2022-09-22 01:27:20 +0200

    :D

--- app/src/main/java/com/hackathon/fipal/Register.java
+++ app/src/main/java/com/hackathon/fipal/Register.java
@@ -11,7 +11,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -19,27 +18,27 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.hackathon.fipal.R;
 
-import org.w3c.dom.Text;
-
-public class LogInActivity extends AppCompatActivity {
+public class Register extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
+        setContentView(R.layout.activity_register);
 
         mAuth = FirebaseAuth.getInstance();
 
-        EditText emailField = (EditText) findViewById(R.id.emailPrompt);
-        EditText passwordField = (EditText) findViewById(R.id.passwordPrompt);
+        EditText emailField = (EditText) findViewById(R.id.reg_emailPrompt);
+        EditText passwordField = (EditText) findViewById(R.id.reg_passwordPrompt);
 
 
 
 
-        Button button = (Button) findViewById(R.id.logIn);
+        Button button = (Button) findViewById(R.id.reg_registration);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,20 +47,21 @@ public class LogInActivity extends AppCompatActivity {
                 String email = emailField.getText().toString();
                 String password = passwordField.getText().toString();
 
-                mAuth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(LogInActivity.this, new OnCompleteListener<AuthResult>() {
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
                                     // Sign in success, update UI with the signed-in user's information
-                                    Log.d(TAG, "signInWithEmail:success");
+                                    Log.d(TAG, "createUserWithEmail:success");
                                     FirebaseUser user = mAuth.getCurrentUser();
-                                    startActivity(new Intent(LogInActivity.this,MainActivity.class));
+                                    startActivity(new Intent(Register.this,LogInActivity.class));
 
                                 } else {
                                     // If sign in fails, display a message to the user.
-                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
-                                    Toast.makeText(LogInActivity.this,"Fail",Toast.LENGTH_LONG).show();
+                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(Register.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
 
                                 }
                             }
@@ -70,18 +70,6 @@ public class LogInActivity extends AppCompatActivity {
             }
         });
 
-        TextView registerText = (TextView) findViewById(R.id.registerText);
-
-        registerText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                startActivity(new Intent(LogInActivity.this,Register.class));
-
-            }
-        });
 
     }
-
-
 }
